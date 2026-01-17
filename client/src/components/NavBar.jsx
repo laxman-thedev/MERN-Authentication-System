a14bbad0148f72cf5de67b83b81ee144fc3fd8f1
laxman-goud <codewithlaxman92@gmail.com>
2026-01-17 10:30:00 +0530

    feat(client): add github star btn in navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -7,10 +7,10 @@ import { toast } from 'react-toastify';
 
 // Top navigation bar with logo, login button, and user menu
 const NavBar = () => {
-    
+
     const navigate = useNavigate();
     const { userData, backendUrl, setUserData, setIsLoggedin } = useContext(AppContext);
-    
+
     // Logout function → clears session and resets state
     const logout = async () => {
         try {
@@ -35,7 +35,7 @@ const NavBar = () => {
                 navigate('/verify-email');
                 toast.success(data.message);
             } else {
-                toast.error(data.message);  
+                toast.error(data.message);
             }
         } catch (error) {
             toast.error(error.message);
@@ -47,29 +47,34 @@ const NavBar = () => {
             {/* App logo */}
             <img src={assets.logo} alt="logo" />
 
-            {userData ? (
-                // If logged in → show profile avatar with dropdown
-                <div className='w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group'>
-                    {userData.name[0].toUpperCase()}
-                    <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10'>
-                        <ul className='list-none m-0 p-2 bg-gray-100 text-sm'>
-                            {/* Verify email option if not verified */}
-                            {!userData.isVerified && (
-                                <li onClick={sendVerificationOtp} className='py-1 px-2 hover:bg-gray-200 cursor-pointer'>Verify email</li>
-                            )}
-                            {/* Logout option */}
-                            <li onClick={logout} className='py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10'>Logout</li>
-                        </ul>
+            <div className="flex gap-4">
+                <a href="https://github.com/laxman-thedev/MERN-Authentication-System" class="max-md:hidden flex gap-1.5 bg-blue-600 text-white btn py-2.5 px-5 rounded-full text-sm hover:bg-white hover:text-blue-600 border hover:border-blue-600" aria-label="GitHub Repository" rel="noopener noreferrer"><svg class="size-5 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M12.006 2a9.847 9.847 0 0 0-6.484 2.44 10.32 10.32 0 0 0-3.393 6.17 10.48 10.48 0 0 0 1.317 6.955 10.045 10.045 0 0 0 5.4 4.418c.504.095.683-.223.683-.494 0-.245-.01-1.052-.014-1.908-2.78.62-3.366-1.21-3.366-1.21a2.711 2.711 0 0 0-1.11-1.5c-.907-.637.07-.621.07-.621.317.044.62.163.885.346.266.183.487.426.647.71.135.253.318.476.538.655a2.079 2.079 0 0 0 2.37.196c.045-.52.27-1.006.635-1.37-2.219-.259-4.554-1.138-4.554-5.07a4.022 4.022 0 0 1 1.031-2.75 3.77 3.77 0 0 1 .096-2.713s.839-.275 2.749 1.05a9.26 9.26 0 0 1 5.004 0c1.906-1.325 2.74-1.05 2.74-1.05.37.858.406 1.828.101 2.713a4.017 4.017 0 0 1 1.029 2.75c0 3.939-2.339 4.805-4.564 5.058a2.471 2.471 0 0 1 .679 1.897c0 1.372-.012 2.477-.012 2.814 0 .272.18.592.687.492a10.05 10.05 0 0 0 5.388-4.421 10.473 10.473 0 0 0 1.313-6.948 10.32 10.32 0 0 0-3.39-6.165A9.847 9.847 0 0 0 12.007 2Z" clip-rule="evenodd"></path></svg>Star On GitHub</a>
+
+                {userData ? (
+                    // If logged in → show profile avatar with dropdown
+                    <div className='w-8 h-8 flex justify-center items-center rounded-full bg-black text-white relative group'>
+                        {userData.name[0].toUpperCase()}
+                        <div className='absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10'>
+                            <ul className='list-none m-0 p-2 bg-gray-100 text-sm'>
+                                {/* Verify email option if not verified */}
+                                {!userData.isVerified && (
+                                    <li onClick={sendVerificationOtp} className='py-1 px-2 hover:bg-gray-200 cursor-pointer'>Verify email</li>
+                                )}
+                                {/* Logout option */}
+                                <li onClick={logout} className='py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10'>Logout</li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            ) : (
-                // If not logged in → show login button
-                <button 
-                    onClick={() => navigate('/login')}
-                    className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all'>
-                    Login <img src={assets.arrow_icon} alt="arrow_icon" />
-                </button>
-            )}
+                ) : (
+                    // If not logged in → show login button
+                    <button
+                        onClick={() => navigate('/login')}
+                        className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all'>
+                        Login <img src={assets.arrow_icon} alt="arrow_icon" />
+                    </button>
+                )}
+            </div>
+
         </div>
     )
 }
