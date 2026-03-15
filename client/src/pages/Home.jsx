d681a530c39a047c13849fa43e7d0e2d44611f1d
laxman-goud <codewithlaxman92@gmail.com>
2026-03-15 20:57:16 +0530

    fix: update Home component layout with background image and flex styling

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 
 const Home = () => {
     return (
-        <div>
+        <div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-center  bg-cover'>
             {/* Navigation bar */}
             <NavBar />
             {/* Hero section */}
